a426a86d0b7fe87d8fc6a00f77329abf4298c538
Steven Warner <stevenwarner@Stevens-MacBook-Pro.local>
2020-10-10 21:13:55 -0700

    fix style and test cms

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -9,13 +9,10 @@ const IndexPagePreview = ({ entry, getAsset }) => {
     return (
       <IndexPageTemplate
         image={getAsset(data.image)}
-        title={data.title}
         heading={data.heading}
         subheading={data.subheading}
-        description={data.description}
-        intro={data.intro || { blurbs: [] }}
-        mainpitch={data.mainpitch || {}}
-        projects={data.projects || {}}
+        projects={data.projects || { projectsList: [] }}
+        // skills={data.skills || { skills: [] }}
       />
     );
   } else {
